22005eab26980f1b01a413c8f3b662f0f4218312
Safa Khan <safa22276@gmail.com>
2026-01-22 17:27:11 +0530

    Comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1); //true
+// console.log(2<1); //false
+// console.log(2>=2); //true
+// console.log(2<=2); //true
+// console.log(2==2); //true
+// console.log(2=="2"); //true (loose equality, only value is compared)
+// console.log(2===2); //true (strict equality, value and type both are compared)
+// console.log(2==="2"); //false
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
