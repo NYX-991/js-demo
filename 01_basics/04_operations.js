b07cec75a15e97f0af4b1d2a3b939579d512ed84
Safa Khan <safa22276@gmail.com>
2026-01-22 16:57:23 +0530

    Operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(2 + 3); //addition
+console.log(2 - 3); //subtraction   
+console.log(2 * 3); //multiplication
+console.log(2 / 3); //division
+console.log(2 % 3); //modulus
+console.log(2 ** 3); //exponentiation
+
+let str1 = "Hello"
+let str2 = "World"
+let str3 = str1 + " " + str2 //string concatenation
+console.log(str3);
+
+console.log("1" + 2); //string concatenation
+console.log(1 + "2"); //string concatenation
+console.log("1" + 2 + 2); //string concatenation
+console.log(1 + 2 + "2"); //string concatenation
+
+console.log(+true)
+console.log(+false)
+
+console.log(+"") //empty string to number
+console.log(+"123") //string to number
+console.log(+"123abc") //NaN
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 3;
+console.log(num1, num2, num3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// read about pre/post increment and decrement operators
