7536fee84a60f2ae800ed0242f1fb1374205587d
Safa Khan <safa22276@gmail.com>
2026-01-22 16:23:52 +0530

    Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,7 +6,7 @@ let age = 30; //number literal
 let isEmployed = true; //boolean literal
 let salary = null; //null literal
 let address; //undefined literal
-let bigNumber = 9007199254740991n; //bigint literal
+let bigNumber = 9007199254740991; //bigint literal
 let uniqueId = Symbol("id"); //symbol literal
 console.log(typeof name); //string
 console.log(typeof age); //number
@@ -15,3 +15,4 @@ console.log(typeof salary); //object (this is a known bug in JS, null is not an 
 console.log(typeof address); //undefined
 console.log(typeof bigNumber); //bigint
 console.log(typeof uniqueId); //symbol
+
